8249d01b21856d45c751106ed4adfc5d934f3b42
RiusL <chithien11294@gmail.com>
2021-10-11 10:46:31 +0700

    first commit

---
+++ DATA.js
@@ -0,0 +1,20 @@
+export const Data = [
+    {
+      id: '123',
+      name: 'Côn Đảo',
+      image:
+        'https://thoidai.com.vn/stores/news_dataimages/lieu.vu/042020/28/11/croped/amp_img/4432_images1562661___o_C_n_S_n__C_n___o__BR_VT__l____i_di_n_hi_m_hoi___ch_u___xu_t_hi_n_trong_danh_s_ch_n_y_c_a_Travel_Leisure..jpg',
+    },
+    {
+      id: '1343',
+      name: 'Vịnh Hạ Long',
+      image:
+        'https://geos-nature.org/wp-content/uploads/2019/10/danh-lam-thang-canh-viet-nam-1-768x432.jpg',
+    },
+    {
+      id: '15153',
+      name: 'Đảo Phú Quốc',
+      image:
+        'https://geos-nature.org/wp-content/uploads/2019/10/danh-lam-thang-canh-viet-nam-3-768x576.jpg',
+    },
+  ];
